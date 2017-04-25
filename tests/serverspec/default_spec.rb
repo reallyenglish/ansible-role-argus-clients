80ed9dd8a966e3f0324b0d753f8e2aa807bf3557
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-04-25 21:14:17 +0900

    create a symlink to the correct SASL plug-ins directory
    fixes #2

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -14,6 +14,14 @@ describe package(package) do
   it { should be_installed }
 end 
 
+case os[:family]
+when "freebsd"
+  describe file("/usr/lib/sasl2") do
+    it { should be_symlink }
+    it { should be_linked_to "/usr/local/lib/sasl2" }
+  end
+end
+
 describe command("ra -h") do
   its(:exit_status) { should eq 1 }
   its(:stdout) { should match(/^Ra Version \d+\.\d+\.\d+(?:\d\.)?/) }
